123dd2f1898f49995dc154f8c4e2ed45cb98de10
Nessim Ferhad <ferhad.nessim@gmail.com>
2019-10-23 16:29:20 +0200

    rectification ou l'article modifié prennait la date du moment de la demande de mise a jour

--- libraries/models/Article.php
+++ libraries/models/Article.php
@@ -17,7 +17,7 @@ class Article extends Model
     
     public function updateArticle(string $title, string $introduction, string $content, int $id): void
     {
-        $query = $this->pdo->prepare('UPDATE articles SET title = :title, introduction = :introduction, content = :content, created_at = NOW() WHERE `id` = :id');
-        $query->execute(compact('title', 'introduction', 'content', 'created_at', 'id'));
+        $query = $this->pdo->prepare('UPDATE articles SET title = :title, introduction = :introduction, content = :content WHERE `id` = :id');
+        $query->execute(compact('title', 'introduction', 'content', 'id'));
     }
 }
